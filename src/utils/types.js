30f6de6452a0f51e4781a00da6b6be8e616bda62
Coreman <androidciudadano@gmail.com>
2018-04-28 20:59:59 -0500

    Changed app name

--- src/utils/types.js
+++ src/utils/types.js
@@ -1,3 +1,3 @@
-export const API_TODO = 'http://192.168.1.2:3205/0528aca6b5ecdcc6a3ed44ae45beb484/todo/api/v1/';
-export const API_AUTH = 'http://192.168.1.2:3205/0528aca6b5ecdcc6a3ed44ae45beb484/auth/api/v1/';
-export const API_NOTES = 'http://192.168.1.2:3205/0528aca6b5ecdcc6a3ed44ae45beb484/notes/api/v1/';
+export const API_TODO = 'http://190.121.135.167/0528aca6b5ecdcc6a3ed44ae45beb484/todo/api/v1/';
+export const API_AUTH = 'http://190.121.135.167/0528aca6b5ecdcc6a3ed44ae45beb484/auth/api/v1/';
+export const API_NOTES = 'http://190.121.135.167/0528aca6b5ecdcc6a3ed44ae45beb484/notes/api/v1/';
